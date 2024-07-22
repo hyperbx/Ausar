86c5253428e99ed1b1337c8f9c0ace015fa155be
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-07-22 16:11:11 +0100

    Remove Toggle Smart Link

--- Ausar/Configuration.cs
+++ Ausar/Configuration.cs
@@ -114,8 +114,6 @@ namespace Ausar
 
         public bool IsToggleSmallerCrosshairScale { get; set; } = false;
 
-        public bool IsToggleSmartLink { get; set; } = true;
-
         public bool IsToggleThirdPersonCamera { get; set; } = false;
 
         public bool IsToggleWorldSpaceViewModel { get; set; } = false;
